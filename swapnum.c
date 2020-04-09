68f5df00ac5a42a6932a6039f0d8de49180b37d5
InvokeGenocide <63400463+InvokeGenocide@users.noreply.github.com>
2020-04-09 16:38:04 +0530

    removed conio and added comments

--- swapnum.c
+++ swapnum.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-#include<conio.h>
 int main()
 {
-  int a=24,b=19;
-  printf("a has %d",a);
+  int a=24,b=19;      //initialising numbers
+  printf("a has %d",a);    // displaying current values
   printf("b has %d",b);
-  a=a+b;
+  a=a+b;     //swapping 
   b=a-b;
   a=a-b;
   printf("Swapping numbers now....");
-  printf("a now has %d",a);
+  printf("a now has %d",a);       //displaying swapped values
   printf("b now has %d",b);
   return 0;
 }
