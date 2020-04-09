d6cbfbe5803e82ac690cb0c8c35af1b51f66a2c4
InvokeGenocide <63400463+InvokeGenocide@users.noreply.github.com>
2020-04-09 16:30:12 +0530

    Created swapnum.c

---
+++ swapnum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a=24,b=19;
+  printf("a has %d",a);
+  printf("b has &d",b);
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  printf("Swapping numbers now....");
+  printf("a now has %d",a);
+  printf("b now has &d",b);
+  return 0;
+}
