c9b82c57a0b0e022219786c46e04acf8dc906365
InvokeGenocide <63400463+InvokeGenocide@users.noreply.github.com>
2020-04-09 16:33:32 +0530

    Edited the printf statements from &d to %d

--- swapnum.c
+++ swapnum.c
@@ -4,12 +4,12 @@ int main()
 {
   int a=24,b=19;
   printf("a has %d",a);
-  printf("b has &d",b);
+  printf("b has %d",b);
   a=a+b;
   b=a-b;
   a=a-b;
   printf("Swapping numbers now....");
   printf("a now has %d",a);
-  printf("b now has &d",b);
+  printf("b now has %d",b);
   return 0;
 }
